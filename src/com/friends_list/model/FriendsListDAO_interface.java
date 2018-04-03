038580e243fdec436ab27a271e6c7d0b8aa807ad
jwes4421 <jwes4421@gmail.com>
2018-04-03 18:20:13 +0800

    FriendsList model

--- src/com/friends_list/model/FriendsListDAO_interface.java
+++ src/com/friends_list/model/FriendsListDAO_interface.java
@@ -1,14 +1,14 @@
 package com.friends_list.model;
 
 import java.util.List;
-import java.util.Set;
 
 public interface FriendsListDAO_interface {
-	public void insert(FriendsListVO frilistVO);
+	public void insert(String self , String other);
 	public void update(FriendsListVO frilistVO);
 	public void delete(String mem_no_self,String mem_no_other); //解除好友
 	public FriendsListVO findByPrimaryKey(String mem_no_self,String mem_no_other);
 	public List<FriendsListVO> getAll();
-	public Set<FriendsListVO> getMemberFriends(String mem_no);
+	public List<FriendsListVO> getMemberFriends(String mem_no);
+	
 	
 }
